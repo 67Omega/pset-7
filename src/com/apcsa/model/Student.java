a1bece39c75fc3bb74ab4012412fc7448497cdcf
Andrew Neuhaus <aneuhaus@ucvts.tec.nj.us>
2019-12-09 09:00:50 -0500

    recognizes user

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -1,15 +1,27 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
 import com.apcsa.model.User;
 
-public class Student extends User {
 
-    private int studentId;
+
+public class Student extends User {
+	
+	private User user;
+	
+	private int studentId;
     private int classRank;
     private int gradeLevel;
     private int graduationYear;
     private double gpa;
     private String firstName;
     private String lastName;
+	
+	public Student(User user, ResultSet rs) {
+		super(user.getUserId(), user.getAccountType(), user.getFirstName(), user.getPassword(), user.getLastLogin());
+	}
+	
+	
     
 }
